c7edf3bdec7ba3a198260f7a39afa95fb313827e
kyoshiyama <kojiyo@uw.edu>
2024-09-08 01:05:57 -0700

    created the framework for the game with basic classes

---
+++ abstractShinobi.java
@@ -0,0 +1,72 @@
+public abstract class Shinobi {
+
+    // each shinobi has a name and picture, as well as a score for each catagory
+    protected String name;
+    protected String picture; // URL or path to an image file
+    protected int body;
+    protected int clan;
+    protected int sensei;
+    protected int talent;
+    protected int mind;
+    protected int summon;
+    protected int chakra;
+    protected int aura;
+
+    // constructor for each unqiue shinobib 
+    public Shinobi(String name, String picture, int body, int clan, int sensei, int talent, int mind, int summon, int chakra, int aura) {
+        this.name = name;
+        this.picture = picture;
+        this.body = body;
+        this.clan = clan;
+        this.sensei = sensei;
+        this.talent = talent;
+        this.mind = mind;
+        this.summon = summon;
+        this.chakra = chakra;
+        this.aura = aura;
+    }
+
+     // Getter methods to look at the score of each catagory
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getClan() {
+        return clan;
+    }
+
+    public int getSensei() {
+        return sensei;
+    }
+
+    public int getTalent() {
+        return talent;
+    }
+
+    public int getMind() {
+        return mind;
+    }
+
+    public int getSummon() {
+        return summon;
+    }
+
+    public int getChakra() {
+        return chakra;
+    }
+
+    public int getAura() {
+        return aura;
+    }
+
+}
---
+++ model.java
@@ -0,0 +1,29 @@
+public Model {
+
+    private int score;
+
+    // this array of strings represents the different catagories, as each shinobi is chosen, add an element to the correct index. 
+    // This array is traversed to determine the score the array is updated via an external array being sent in from the controller
+    private String[] MyChosenShinobis= new String[8];
+
+
+    // this hashmap contains all the different shinobis that will be used in this game. this map is used to 
+    // look up the specific shinobis to them access their point values
+    private HashMap<String, Shinobi> shinobiGuide = new HashMap<>();
+
+
+    public Model() {
+        // initialize all shinobis here and add to an arraylist
+    }
+
+    public int calculateCurrentScore() {
+        // traverse the array and calculate the current score here 
+    }
+
+    public String[] recieveUpdatedArray(String[] theUpdatedArray) {
+        // this method is called by the controller and sends in the updated array from the view after each selection
+        MyChosenShinobis = theUpdatedArray;
+    }
+
+
+}
